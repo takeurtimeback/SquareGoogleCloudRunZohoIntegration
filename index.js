341f5d18a62a74f64f76ae03d8deed3d6b925e8e
daytonabowen <115041272+daytonabowen@users.noreply.github.com>
2025-12-18 07:47:51 -0800

    ij

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function getZohoAccessToken(clientId, clientSecret, refreshToken) {
 async function createSalesReceipt(accessToken, receiptData) {
   const response = await axios.post(
     'https://books.zoho.com/api/v3/salesreceipts',
-    receiptData,
+    {"customer_name":"test","payment_method":"cash","line_items":[{"name":"Beer","quantity":1,"rate":5.13}]},
     {
       headers: {
         Authorization: `Zoho-oauthtoken ${accessToken}`,
